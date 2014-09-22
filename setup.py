2cb04c94c68fd019e6e1d0df4ffaeca987314c1b
Justin Abrahms <justin@abrah.ms>
2014-09-22 05:49:50 -0700

    easy setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='account_reports',
+    version='0.1.0',
+    packages=find_packages(),
+    url='https://github.com/sprintly/account_reports',
+    author='Justin Abrahms',
+    author_email='jabrahms@quickleft.com',
+    description='Library for handling KPI account reports',
+)
