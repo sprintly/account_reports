996ae4959e9e6dcf79653445ceeff91aac485851
Justin Abrahms <justin@abrah.ms>
2014-10-06 12:57:42 -0700

    Apparently, packages with underscores should be dashes in name.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='account_reports',
+    name='account-reports',
     version='0.2.0',
     packages=find_packages(),
     url='https://github.com/sprintly/account_reports',
